bdffc01c0273dd97adb81f986fad59d843219a96
valonkadriaj <valon_kadriaj@outlook.com>
2020-05-16 19:40:40 +0200

    fix import

--- src/pages/sigin-and-sign-up/sigin-and-sign-up.component.js
+++ src/pages/sigin-and-sign-up/sigin-and-sign-up.component.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import SignIn from '../../../components/sign-in/sign-in.component';
+import SignIn from '../../components/sign-in/sign-in.component';
 import './sigin-and-sign-up.style.scss';
-import SignUp from '../../../components/sign-up/sign-up.component';
+import SignUp from '../../components/sign-up/sign-up.component';
 
 const SignInAndSignupPage = () => (
   <div className="sigin-and-sign-up">
